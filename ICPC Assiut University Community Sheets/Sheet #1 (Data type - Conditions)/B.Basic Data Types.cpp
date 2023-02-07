23d536e1e25194c4b430c6fe738527ae982e9392
Mohammed Helal <124520038+Mhammedhelal@users.noreply.github.com>
2023-02-07 20:03:13 +0200

    Create B.Basic Data Types.cpp

---
+++ ICPC Assiut University Community Sheets/Sheet #1 (Data type - Conditions)/B.Basic Data Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int i;
+	long long g;
+	char c;
+	float f;
+	double d;
+	cin >> i >> g >> c >> f >> d;
+	cout << i << endl
+		 << g << endl
+		 << c << endl
+		 << f << endl
+		 << d << endl;
+
+	return 0;
+}
