0a2a992ec739f3d20359fd76f791727f3f8ccce1
Mohammed Helal <124520038+Mhammedhelal@users.noreply.github.com>
2023-02-07 21:25:54 +0200

    Create O. Calculator.cpp

---
+++ ICPC Assiut University Community Sheets/Sheet #1 (Data type - Conditions)/O. Calculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	char s;
+	cin >> a >> s >> b;
+
+	switch (s)
+	{
+	case '+':
+		cout << a + b;
+		break;
+	case '-':
+		cout << a - b;
+		break;
+	case '*':
+		cout << a * b;
+		break;
+	case '/':
+		cout << a / b;
+		break;
+	}
+
+	return 0;
+}
