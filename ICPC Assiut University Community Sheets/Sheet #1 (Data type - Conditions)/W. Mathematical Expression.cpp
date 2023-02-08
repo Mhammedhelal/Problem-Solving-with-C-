e8e4980ff4f8f41b7ce16d93d4df92c495eb06bf
Mohammed Helal <124520038+Mhammedhelal@users.noreply.github.com>
2023-02-08 08:21:20 +0200

    Create W. Mathematical Expression.cpp

---
+++ ICPC Assiut University Community Sheets/Sheet #1 (Data type - Conditions)/W. Mathematical Expression.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	char s, q;
+	cin >> a >> s >> b >> q >> c;
+
+	if (s == '+')
+	{
+		(a + b == c) ? cout << "Yes" : cout << a + b;
+	}
+	else if (s == '-')
+	{
+		(a - b == c) ? cout << "Yes" : cout << a - b;
+	}
+	else if (s == '*')
+	{
+		(a * b == c) ? cout << "Yes" : cout << a * b;
+	}
+
+	return 0;
+}
