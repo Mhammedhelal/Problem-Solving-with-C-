939118a6b37c7ce8670606cce9547535e52c8cd4
Mohammed Helal <124520038+Mhammedhelal@users.noreply.github.com>
2023-02-08 11:19:27 +0200

    Update J. Primes from 1 to n.cpp

--- ICPC Assiut University Community Sheets/Sheet #2 (Loops)/J. Primes from 1 to n.cpp
+++ ICPC Assiut University Community Sheets/Sheet #2 (Loops)/J. Primes from 1 to n.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+bool isPrime(int x)
+{
+	if (x == 1)
+		return false;
+	else
+	{
+		int factors = 1;
+		for (int i = 2; i <= x; i++)
+		{
+			if (x % i == 0)
+				factors++;
+		}
+		if (factors == 2)
+			return true;
+		else
+			return false;
+	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		if (isPrime(i))
+			cout << i << " ";
+	}
+
+	return 0;
+}
