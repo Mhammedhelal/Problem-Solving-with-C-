08f3d737d20df064333f4cf57a4ef86a9621ed66
Mohammed Helal <124520038+Mhammedhelal@users.noreply.github.com>
2023-02-09 11:26:22 +0200

    Create I. Smallest Pair.cpp

---
+++ ICPC Assiut University Community Sheets/Sheet #3 (Arrays)/I. Smallest Pair.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int testCases, result = 0, flag = 1, min;
+	cin >> testCases;
+	while (testCases--)
+	{
+		int size;
+		cin >> size;
+		int *arr = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			cin >> arr[i];
+		}
+		for (int z = 1; z <= size; z++)
+		{
+			for (int x = z + 1; x <= size; x++)
+			{
+				result = (arr[z - 1] + arr[x - 1]) + (x - z);
+				if (flag == 0 && min > result)
+				{
+					min = result;
+				}
+				if (flag)
+				{
+					min = result;
+					flag = 0;
+				}
+				result = 0;
+			}
+		}
+		flag = 1;
+		cout << min << endl;
+	}
+}
