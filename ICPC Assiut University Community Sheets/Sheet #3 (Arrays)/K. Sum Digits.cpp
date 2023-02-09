694a8a151299d9bb73913acc2e1179b391f93ff7
Mohammed Helal <124520038+Mhammedhelal@users.noreply.github.com>
2023-02-09 11:31:43 +0200

    Create K. Sum Digits.cpp

---
+++ ICPC Assiut University Community Sheets/Sheet #3 (Arrays)/K. Sum Digits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+    string value;
+    cin>>value;
+    long long sum =0;
+    for (int i = 0; i < size; i++)
+    {
+        sum+=value[i]-'0';
+    }
+    cout << sum;
+	
+	return 0;
+}
