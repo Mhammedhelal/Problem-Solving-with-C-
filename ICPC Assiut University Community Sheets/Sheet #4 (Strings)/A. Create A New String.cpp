8ef6cc7a3d7bdbac4fdf7c4514ca24d12a6ef610
Mohammed Helal <124520038+Mhammedhelal@users.noreply.github.com>
2023-02-10 07:09:16 +0200

    Create A. Create A New String.cpp

---
+++ ICPC Assiut University Community Sheets/Sheet #4 (Strings)/A. Create A New String.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s, t;
+	cin >> s >> t;
+	cout << size(s) << " " << size(t) << endl;
+	cout << s << " " << t;
+
+	return 0;
+}
