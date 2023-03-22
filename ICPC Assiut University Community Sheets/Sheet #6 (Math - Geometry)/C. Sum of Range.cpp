20d9c696fcd5d62500e78230a91c47a2b2f70b67
Mohammed Helal <124520038+Mhammedhelal@users.noreply.github.com>
2023-03-22 13:08:47 +0200

    Update C. Sum of Range.cpp

--- ICPC Assiut University Community Sheets/Sheet #6 (Math - Geometry)/C. Sum of Range.cpp
+++ ICPC Assiut University Community Sheets/Sheet #6 (Math - Geometry)/C. Sum of Range.cpp
@@ -3,23 +3,24 @@
 #include <math.h>
 using namespace std;
 
-long long even_sum(long long x)
-{
-	return x * (x + 1);
+long long sumOdd(long long n) {
+	long long res = (n + 1) / 2;
+	long long finalRes = res * res;
+	return finalRes;
 }
-long long odd_sum(long long x)
-{
-	return pow((x + 1) / 2, 2);
+long long sumEven(long long n) {
+	long long res = (n * (n + 1));
+	return res;
 }
-int main()
-{
+int main() {
 	int a, b;
 	cin >> a >> b;
-	long long evenRes = even_sum(max(a, b) / 2) - even_sum((min(a, b) - 1) / 2);
-	long long oddRes = odd_sum(max(a, b)) - odd_sum(min(a, b) - 1);
+	int start, end;
+	start = min(a, b);
+	end = max(a, b);
+	long long evenRes = sumEven(end / 2) - sumEven((start - 1) / 2);
+	long long oddRes = sumOdd(end) - sumOdd(start - 1);
 	cout << evenRes + oddRes << endl;
 	cout << evenRes << endl;
 	cout << oddRes << endl;
-
-	return 0;
 }
